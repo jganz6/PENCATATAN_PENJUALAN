f70f3dfeffae502a5967a9f6e8ba03c608e94a9c
jganz6 <jodiemanopo28@gmail.com>
2024-02-18 10:48:06 +0700

    init

---
+++ PENCATATAN_PENJUALAN/FormPencatatanPenjualan.Designer.vb
@@ -0,0 +1,363 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class FormPencatatanPenjualan
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FormPencatatanPenjualan))
+        DataGridViewPenjualan = New DataGridView()
+        ColumnItem = New DataGridViewTextBoxColumn()
+        ColumnQuantity = New DataGridViewTextBoxColumn()
+        ColumnPrice = New DataGridViewTextBoxColumn()
+        ColumnTotal = New DataGridViewTextBoxColumn()
+        ButtonTambah = New Button()
+        TextBoxItem = New TextBox()
+        TextBoxQuantity = New TextBox()
+        TextBoxPrice = New TextBox()
+        ButtonHapus = New Button()
+        LabelTotalPrice = New Label()
+        LabelTotalPriceQ = New Label()
+        GroupBoxDetailTrx = New GroupBox()
+        ButtonPrintCst = New Button()
+        LabelIdTrxValue = New Label()
+        LabelCreatedAtValue = New Label()
+        LabelCreatedAt = New Label()
+        LabelIdTrx = New Label()
+        OpenFileDialog1 = New OpenFileDialog()
+        FolderBrowserDialog1 = New FolderBrowserDialog()
+        ButtonOpenTrx = New Button()
+        LabelFileDirName = New Label()
+        ButtonNewTrx = New Button()
+        LabePath = New Label()
+        GroupBoxTrx = New GroupBox()
+        ButtonCloseTrx = New Button()
+        PrintDocument1 = New Printing.PrintDocument()
+        PrintPreviewDialog1 = New PrintPreviewDialog()
+        CType(DataGridViewPenjualan, ComponentModel.ISupportInitialize).BeginInit()
+        GroupBoxDetailTrx.SuspendLayout()
+        GroupBoxTrx.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' DataGridViewPenjualan
+        ' 
+        DataGridViewPenjualan.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridViewPenjualan.Columns.AddRange(New DataGridViewColumn() {ColumnItem, ColumnQuantity, ColumnPrice, ColumnTotal})
+        DataGridViewPenjualan.Location = New Point(18, 75)
+        DataGridViewPenjualan.Name = "DataGridViewPenjualan"
+        DataGridViewPenjualan.Size = New Size(735, 213)
+        DataGridViewPenjualan.TabIndex = 0
+        ' 
+        ' ColumnItem
+        ' 
+        ColumnItem.HeaderText = "Item"
+        ColumnItem.Name = "ColumnItem"
+        ' 
+        ' ColumnQuantity
+        ' 
+        ColumnQuantity.HeaderText = "Quantity"
+        ColumnQuantity.Name = "ColumnQuantity"
+        ' 
+        ' ColumnPrice
+        ' 
+        ColumnPrice.HeaderText = "Price"
+        ColumnPrice.Name = "ColumnPrice"
+        ' 
+        ' ColumnTotal
+        ' 
+        ColumnTotal.AutoSizeMode = DataGridViewAutoSizeColumnMode.None
+        ColumnTotal.HeaderText = "Total"
+        ColumnTotal.Name = "ColumnTotal"
+        ' 
+        ' ButtonTambah
+        ' 
+        ButtonTambah.BackColor = SystemColors.ActiveCaption
+        ButtonTambah.Location = New Point(352, 296)
+        ButtonTambah.Name = "ButtonTambah"
+        ButtonTambah.Size = New Size(82, 23)
+        ButtonTambah.TabIndex = 1
+        ButtonTambah.Text = "Tambah"
+        ButtonTambah.UseVisualStyleBackColor = False
+        ' 
+        ' TextBoxItem
+        ' 
+        TextBoxItem.Location = New Point(18, 296)
+        TextBoxItem.Name = "TextBoxItem"
+        TextBoxItem.PlaceholderText = "item"
+        TextBoxItem.Size = New Size(100, 23)
+        TextBoxItem.TabIndex = 2
+        ' 
+        ' TextBoxQuantity
+        ' 
+        TextBoxQuantity.Location = New Point(124, 296)
+        TextBoxQuantity.Name = "TextBoxQuantity"
+        TextBoxQuantity.PlaceholderText = "qty"
+        TextBoxQuantity.Size = New Size(100, 23)
+        TextBoxQuantity.TabIndex = 3
+        ' 
+        ' TextBoxPrice
+        ' 
+        TextBoxPrice.Location = New Point(230, 296)
+        TextBoxPrice.Name = "TextBoxPrice"
+        TextBoxPrice.PlaceholderText = "price"
+        TextBoxPrice.Size = New Size(100, 23)
+        TextBoxPrice.TabIndex = 4
+        ' 
+        ' ButtonHapus
+        ' 
+        ButtonHapus.BackColor = Color.Red
+        ButtonHapus.ForeColor = SystemColors.ButtonHighlight
+        ButtonHapus.Location = New Point(18, 333)
+        ButtonHapus.Name = "ButtonHapus"
+        ButtonHapus.Size = New Size(75, 23)
+        ButtonHapus.TabIndex = 5
+        ButtonHapus.Text = "Hapus"
+        ButtonHapus.UseVisualStyleBackColor = False
+        ' 
+        ' LabelTotalPrice
+        ' 
+        LabelTotalPrice.AutoSize = True
+        LabelTotalPrice.Font = New Font("Segoe UI", 14F)
+        LabelTotalPrice.Location = New Point(539, 291)
+        LabelTotalPrice.Name = "LabelTotalPrice"
+        LabelTotalPrice.Size = New Size(108, 25)
+        LabelTotalPrice.TabIndex = 6
+        LabelTotalPrice.Text = "Total Price :"
+        ' 
+        ' LabelTotalPriceQ
+        ' 
+        LabelTotalPriceQ.AutoSize = True
+        LabelTotalPriceQ.Font = New Font("Segoe UI", 12F)
+        LabelTotalPriceQ.Location = New Point(653, 294)
+        LabelTotalPriceQ.Name = "LabelTotalPriceQ"
+        LabelTotalPriceQ.Size = New Size(19, 21)
+        LabelTotalPriceQ.TabIndex = 7
+        LabelTotalPriceQ.Text = "0"
+        ' 
+        ' GroupBoxDetailTrx
+        ' 
+        GroupBoxDetailTrx.Controls.Add(ButtonPrintCst)
+        GroupBoxDetailTrx.Controls.Add(LabelIdTrxValue)
+        GroupBoxDetailTrx.Controls.Add(LabelCreatedAtValue)
+        GroupBoxDetailTrx.Controls.Add(LabelCreatedAt)
+        GroupBoxDetailTrx.Controls.Add(LabelTotalPriceQ)
+        GroupBoxDetailTrx.Controls.Add(LabelIdTrx)
+        GroupBoxDetailTrx.Controls.Add(LabelTotalPrice)
+        GroupBoxDetailTrx.Controls.Add(ButtonHapus)
+        GroupBoxDetailTrx.Controls.Add(TextBoxPrice)
+        GroupBoxDetailTrx.Controls.Add(TextBoxQuantity)
+        GroupBoxDetailTrx.Controls.Add(TextBoxItem)
+        GroupBoxDetailTrx.Controls.Add(ButtonTambah)
+        GroupBoxDetailTrx.Controls.Add(DataGridViewPenjualan)
+        GroupBoxDetailTrx.Location = New Point(12, 169)
+        GroupBoxDetailTrx.Name = "GroupBoxDetailTrx"
+        GroupBoxDetailTrx.Size = New Size(776, 373)
+        GroupBoxDetailTrx.TabIndex = 9
+        GroupBoxDetailTrx.TabStop = False
+        GroupBoxDetailTrx.Text = "Detail Transaction"
+        GroupBoxDetailTrx.Visible = False
+        ' 
+        ' ButtonPrintCst
+        ' 
+        ButtonPrintCst.BackColor = SystemColors.ActiveCaption
+        ButtonPrintCst.Location = New Point(678, 333)
+        ButtonPrintCst.Name = "ButtonPrintCst"
+        ButtonPrintCst.Size = New Size(75, 23)
+        ButtonPrintCst.TabIndex = 13
+        ButtonPrintCst.Text = "Print"
+        ButtonPrintCst.UseVisualStyleBackColor = False
+        ' 
+        ' LabelIdTrxValue
+        ' 
+        LabelIdTrxValue.AutoSize = True
+        LabelIdTrxValue.Location = New Point(63, 37)
+        LabelIdTrxValue.Name = "LabelIdTrxValue"
+        LabelIdTrxValue.Size = New Size(12, 15)
+        LabelIdTrxValue.TabIndex = 12
+        LabelIdTrxValue.Text = "-"
+        ' 
+        ' LabelCreatedAtValue
+        ' 
+        LabelCreatedAtValue.AutoSize = True
+        LabelCreatedAtValue.Location = New Point(625, 37)
+        LabelCreatedAtValue.Name = "LabelCreatedAtValue"
+        LabelCreatedAtValue.Size = New Size(12, 15)
+        LabelCreatedAtValue.TabIndex = 11
+        LabelCreatedAtValue.Text = "-"
+        ' 
+        ' LabelCreatedAt
+        ' 
+        LabelCreatedAt.AutoSize = True
+        LabelCreatedAt.Font = New Font("Segoe UI", 10F)
+        LabelCreatedAt.Location = New Point(538, 33)
+        LabelCreatedAt.Name = "LabelCreatedAt"
+        LabelCreatedAt.Size = New Size(86, 19)
+        LabelCreatedAt.TabIndex = 10
+        LabelCreatedAt.Text = "Created At : "
+        ' 
+        ' LabelIdTrx
+        ' 
+        LabelIdTrx.AutoSize = True
+        LabelIdTrx.Font = New Font("Segoe UI", 10F)
+        LabelIdTrx.Location = New Point(27, 33)
+        LabelIdTrx.Name = "LabelIdTrx"
+        LabelIdTrx.Size = New Size(30, 19)
+        LabelIdTrx.TabIndex = 4
+        LabelIdTrx.Text = "ID :"
+        ' 
+        ' OpenFileDialog1
+        ' 
+        OpenFileDialog1.FileName = "OpenFileDialog1"
+        OpenFileDialog1.InitialDirectory = "C:\JenieWorkStation\DATA_TRANSAKSI_PROGVISUAL_UAS"
+        ' 
+        ' FolderBrowserDialog1
+        ' 
+        FolderBrowserDialog1.InitialDirectory = "C:\JenieWorkStation\DATA_TRANSAKSI_PROGVISUAL_UAS"
+        ' 
+        ' ButtonOpenTrx
+        ' 
+        ButtonOpenTrx.BackColor = SystemColors.ActiveCaption
+        ButtonOpenTrx.Font = New Font("Segoe UI", 10F)
+        ButtonOpenTrx.Location = New Point(27, 55)
+        ButtonOpenTrx.Name = "ButtonOpenTrx"
+        ButtonOpenTrx.Size = New Size(92, 31)
+        ButtonOpenTrx.TabIndex = 0
+        ButtonOpenTrx.Text = "Select File"
+        ButtonOpenTrx.UseVisualStyleBackColor = False
+        ' 
+        ' LabelFileDirName
+        ' 
+        LabelFileDirName.AutoSize = True
+        LabelFileDirName.BackColor = SystemColors.AppWorkspace
+        LabelFileDirName.Font = New Font("Segoe UI", 9F)
+        LabelFileDirName.Location = New Point(70, 28)
+        LabelFileDirName.Name = "LabelFileDirName"
+        LabelFileDirName.Size = New Size(0, 15)
+        LabelFileDirName.TabIndex = 1
+        ' 
+        ' ButtonNewTrx
+        ' 
+        ButtonNewTrx.BackColor = SystemColors.ActiveCaption
+        ButtonNewTrx.Font = New Font("Segoe UI", 10F)
+        ButtonNewTrx.Location = New Point(125, 55)
+        ButtonNewTrx.Name = "ButtonNewTrx"
+        ButtonNewTrx.Size = New Size(100, 31)
+        ButtonNewTrx.TabIndex = 2
+        ButtonNewTrx.Text = "New"
+        ButtonNewTrx.UseVisualStyleBackColor = False
+        ' 
+        ' LabePath
+        ' 
+        LabePath.AutoSize = True
+        LabePath.Font = New Font("Segoe UI", 9F)
+        LabePath.Location = New Point(27, 28)
+        LabePath.Name = "LabePath"
+        LabePath.Size = New Size(37, 15)
+        LabePath.TabIndex = 3
+        LabePath.Text = "Path :"
+        ' 
+        ' GroupBoxTrx
+        ' 
+        GroupBoxTrx.Controls.Add(ButtonCloseTrx)
+        GroupBoxTrx.Controls.Add(LabePath)
+        GroupBoxTrx.Controls.Add(ButtonNewTrx)
+        GroupBoxTrx.Controls.Add(LabelFileDirName)
+        GroupBoxTrx.Controls.Add(ButtonOpenTrx)
+        GroupBoxTrx.Font = New Font("Segoe UI", 14F)
+        GroupBoxTrx.ForeColor = SystemColors.ControlText
+        GroupBoxTrx.Location = New Point(12, 12)
+        GroupBoxTrx.Name = "GroupBoxTrx"
+        GroupBoxTrx.Size = New Size(776, 132)
+        GroupBoxTrx.TabIndex = 8
+        GroupBoxTrx.TabStop = False
+        GroupBoxTrx.Text = "Transaction"
+        ' 
+        ' ButtonCloseTrx
+        ' 
+        ButtonCloseTrx.BackColor = Color.Red
+        ButtonCloseTrx.Enabled = False
+        ButtonCloseTrx.Font = New Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        ButtonCloseTrx.ForeColor = SystemColors.ButtonFace
+        ButtonCloseTrx.Location = New Point(30, 92)
+        ButtonCloseTrx.Name = "ButtonCloseTrx"
+        ButtonCloseTrx.Size = New Size(88, 32)
+        ButtonCloseTrx.TabIndex = 4
+        ButtonCloseTrx.Text = "Close File"
+        ButtonCloseTrx.UseVisualStyleBackColor = False
+        ButtonCloseTrx.Visible = False
+        ' 
+        ' PrintPreviewDialog1
+        ' 
+        PrintPreviewDialog1.AutoScrollMargin = New Size(0, 0)
+        PrintPreviewDialog1.AutoScrollMinSize = New Size(0, 0)
+        PrintPreviewDialog1.ClientSize = New Size(400, 300)
+        PrintPreviewDialog1.Document = PrintDocument1
+        PrintPreviewDialog1.Enabled = True
+        PrintPreviewDialog1.Icon = CType(resources.GetObject("PrintPreviewDialog1.Icon"), Icon)
+        PrintPreviewDialog1.Name = "PrintPreviewDialog1"
+        PrintPreviewDialog1.Visible = False
+        ' 
+        ' FormPencatatanPenjualan
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 554)
+        Controls.Add(GroupBoxDetailTrx)
+        Controls.Add(GroupBoxTrx)
+        Name = "FormPencatatanPenjualan"
+        Text = "Form Pencatatan Penjualan"
+        CType(DataGridViewPenjualan, ComponentModel.ISupportInitialize).EndInit()
+        GroupBoxDetailTrx.ResumeLayout(False)
+        GroupBoxDetailTrx.PerformLayout()
+        GroupBoxTrx.ResumeLayout(False)
+        GroupBoxTrx.PerformLayout()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents DataGridViewPenjualan As DataGridView
+    Friend WithEvents ButtonTambah As Button
+    Friend WithEvents TextBoxItem As TextBox
+    Friend WithEvents TextBoxQuantity As TextBox
+    Friend WithEvents TextBoxPrice As TextBox
+    Friend WithEvents ButtonHapus As Button
+    Friend WithEvents ColumnItem As DataGridViewTextBoxColumn
+    Friend WithEvents ColumnQuantity As DataGridViewTextBoxColumn
+    Friend WithEvents ColumnPrice As DataGridViewTextBoxColumn
+    Friend WithEvents ColumnTotal As DataGridViewTextBoxColumn
+    Friend WithEvents LabelTotalPrice As Label
+    Friend WithEvents LabelTotalPriceQ As Label
+    Friend WithEvents GroupBoxDetailTrx As GroupBox
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents LabelIdTrx As Label
+    Friend WithEvents LabelCreatedAt As Label
+    Friend WithEvents LabelIdTrxValue As Label
+    Friend WithEvents LabelCreatedAtValue As Label
+    Friend WithEvents ButtonOpenTrx As Button
+    Friend WithEvents LabelFileDirName As Label
+    Friend WithEvents ButtonNewTrx As Button
+    Friend WithEvents LabePath As Label
+    Friend WithEvents GroupBoxTrx As GroupBox
+    Friend WithEvents ButtonCloseTrx As Button
+    Friend WithEvents ButtonPrintCst As Button
+    Friend WithEvents PrintDocument1 As Printing.PrintDocument
+    Friend WithEvents PrintPreviewDialog1 As PrintPreviewDialog
+
+End Class
---
+++ PENCATATAN_PENJUALAN/FormPencatatanPenjualan.vb
@@ -0,0 +1,171 @@
+﻿Imports System.Drawing.Printing
+Imports System.IO
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+Imports System.Xml
+
+Public Class FormPencatatanPenjualan
+    Dim pathNew As String = ""
+
+    Private Sub ButtonCloseTrxVisible(cond As Boolean)
+        ButtonCloseTrx.Visible = cond
+        ButtonCloseTrx.Enabled = cond
+    End Sub
+
+    Private Sub ControlGroupBoxDetailTrxVisibility(cond As Boolean)
+        GroupBoxDetailTrx.Visible = cond
+        GroupBoxDetailTrx.Enabled = cond
+    End Sub
+
+    Private Sub CleanCurrentFileTrx()
+        pathNew = ""
+        LabelFileDirName.Text = ""
+        LabelCreatedAtValue.Text = "-"
+        LabelIdTrxValue.Text = "-"
+        ControlGroupBoxDetailTrxVisibility(False)
+    End Sub
+    Private Sub LoadData()
+        If pathNew <> "" And File.Exists(pathNew) Then
+            ControlGroupBoxDetailTrxVisibility(True)
+            ButtonCloseTrxVisible(True)
+            DataGridViewPenjualan.Rows.Clear()
+            Using reader As StreamReader = New StreamReader(pathNew)
+                Dim totalPrice As Integer = 0
+                Do While reader.Peek() >= 0
+                    Dim line As String = reader.ReadLine()
+                    Dim data As String() = line.Split(","c)
+                    Dim totalRowsPrice = Convert.ToInt64(data(1)) * Convert.ToInt64(data(2))
+                    ReDim Preserve data(data.Length) ' Increase the size by 1
+                    totalPrice += totalRowsPrice
+                    data(data.Length - 1) = totalRowsPrice.ToString()
+                    DataGridViewPenjualan.Rows.Add(data)
+                Loop
+                LabelTotalPriceQ.Text = totalPrice
+            End Using
+        Else
+            ControlGroupBoxDetailTrxVisibility(False)
+            ButtonCloseTrxVisible(False)
+        End If
+    End Sub
+
+    Private Sub ButtonTambah_Click(sender As Object, e As EventArgs) Handles ButtonTambah.Click
+        Dim newItem As String = TextBoxItem.Text.Trim()
+        Dim newQuantity As String = TextBoxQuantity.Text.Trim()
+        Dim newPrice As String = TextBoxPrice.Text.Trim()
+
+        If Not String.IsNullOrEmpty(newItem) AndAlso Not String.IsNullOrEmpty(newQuantity) AndAlso Not String.IsNullOrEmpty(newPrice) Then
+            Using writer As StreamWriter = New StreamWriter(pathNew, True)
+                writer.WriteLine($"{newItem},{newQuantity},{newPrice}")
+            End Using
+            TextBoxItem.Clear()
+            TextBoxQuantity.Clear()
+            TextBoxPrice.Clear()
+            LoadData()
+        Else
+            MessageBox.Show("Silakan masukkan item, kuantitas, dan harga terlebih dahulu.", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        End If
+    End Sub
+
+    Private Sub ButtonHapus_Click(sender As Object, e As EventArgs) Handles ButtonHapus.Click
+        If DataGridViewPenjualan.SelectedRows.Count > 0 Then
+            Dim selectedIndex As Integer = DataGridViewPenjualan.SelectedRows(0).Index
+
+            Dim lines As List(Of String) = File.ReadAllLines(pathNew).ToList()
+            lines.RemoveAt(selectedIndex)
+            File.WriteAllLines(pathNew, lines)
+            LoadData()
+        Else
+            MessageBox.Show("Silakan pilih catatan penjualan yang akan dihapus.", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        End If
+    End Sub
+
+    Private Sub ButtonOpenTrx_Click(sender As Object, e As EventArgs) Handles ButtonOpenTrx.Click
+        OpenFileDialog1.Filter = "TEXT FILE | *.txt"
+        If OpenFileDialog1.ShowDialog = DialogResult.OK Then
+            LabelFileDirName.Text = OpenFileDialog1.FileName
+            pathNew = OpenFileDialog1.FileName
+            LabelCreatedAtValue.Text = GetCreatedTime_FromFile(pathNew)
+            LabelIdTrxValue.Text = Path.GetFileNameWithoutExtension(pathNew)
+            LoadData()
+        End If
+    End Sub
+
+    Private Sub ButtonNewTrx_Click(sender As Object, e As EventArgs) Handles ButtonNewTrx.Click
+        If (FolderBrowserDialog1.ShowDialog() = DialogResult.OK) Then
+            Dim uuidNew As Guid = Guid.NewGuid()
+            pathNew = FolderBrowserDialog1.SelectedPath + "\" + uuidNew.ToString() + ".txt"
+            LabelFileDirName.Text = pathNew
+            File.Create(pathNew).Close()
+            LabelCreatedAtValue.Text = GetCreatedTime_FromFile(pathNew)
+            LabelIdTrxValue.Text = Path.GetFileNameWithoutExtension(pathNew)
+            LoadData()
+        End If
+    End Sub
+    Private Function GetCreatedTime_FromFile(ByVal path As String) As String
+        Dim fileCreatedDate As DateTime = File.GetCreationTime(path)
+
+        Return fileCreatedDate.ToString()
+    End Function
+
+    Private Sub ButtonCloseTrx_Click(sender As Object, e As EventArgs) Handles ButtonCloseTrx.Click
+        ButtonCloseTrxVisible(False)
+        CleanCurrentFileTrx()
+    End Sub
+
+    Private Sub ButtonPrintCst_Click(sender As Object, e As EventArgs) Handles ButtonPrintCst.Click
+        Dim printDialog As New PrintDialog()
+        If printDialog.ShowDialog() = DialogResult.OK Then
+            ' Set the PrintDocument for printing
+            PrintDocument1.PrinterSettings = printDialog.PrinterSettings
+            ' Subscribe to the PrintPage event
+            AddHandler PrintDocument1.PrintPage, AddressOf PrintPageHandler
+            If PrintPreviewDialog1.ShowDialog() = DialogResult.OK Then
+                ' Start printing
+                PrintDocument1.Print()
+            End If
+            ' Unsubscribe from the PrintPage event
+            RemoveHandler PrintDocument1.PrintPage, AddressOf PrintPageHandler
+        End If
+    End Sub
+
+    Private Sub PrintPageHandler(sender As Object, e As PrintPageEventArgs)
+        Dim bm As New Bitmap(Me.DataGridViewPenjualan.Width, Me.DataGridViewPenjualan.Height)
+        DataGridViewPenjualan.DrawToBitmap(bm, New Rectangle(30, 30, Me.DataGridViewPenjualan.Width, Me.DataGridViewPenjualan.Height))
+
+        ' Font for the table
+        Dim font As New Font("Arial", 12, FontStyle.Regular, GraphicsUnit.Point)
+        ' Margin and padding
+        Dim margin As Integer = 40
+        Dim padding As Integer = 5
+        ' Line height
+        Dim lineHeight As Integer = font.Height + padding
+
+        ' Coordinates for drawing
+        Dim x As Single = margin
+        Dim y As Single = margin
+
+
+        ' Draw headers
+        Dim IdTrxText As String = "ID : " + LabelIdTrxValue.Text
+        Dim TimeStamptzTrxText As String = "Created At : " + LabelCreatedAtValue.Text
+        Dim TotalTrxText As String = "Total Price : " + LabelTotalPriceQ.Text
+        e.Graphics.DrawString(IdTrxText, font, Brushes.Black, x, y)
+        y += lineHeight
+        x = margin
+        e.Graphics.DrawString(TimeStamptzTrxText, font, Brushes.Black, x, y)
+        y += lineHeight
+        x = margin
+        e.Graphics.DrawString(TotalTrxText, font, Brushes.Black, x, y)
+
+        ' Draw data
+        y += lineHeight
+        e.Graphics.DrawImage(bm, x, y)
+        y += DataGridViewPenjualan.Height + lineHeight
+
+        ' Check if another page is needed
+        If y + lineHeight < e.MarginBounds.Bottom Then
+            e.HasMorePages = False
+        Else
+            e.HasMorePages = True
+        End If
+    End Sub
+End Class
